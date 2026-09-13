2fd5b2011b8a0ce350178d9d33903577c5e8837e
Bikash <aayushbashyal92@gmail.com>
2026-09-13 13:20:02 +0545

    adding sapphire block and correcting error

--- src/main/java/net/bikash/miningmod/block/ModBlocks.java
+++ src/main/java/net/bikash/miningmod/block/ModBlocks.java
@@ -6,7 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;S
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;

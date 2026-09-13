f15cc7b963a99c0d5edfb0f32d6d87125dd6c8c9
Bikash <aayushbashyal92@gmail.com>
2026-09-13 20:27:58 +0545

    adding looting

--- src/main/java/net/bikash/miningmod/block/ModBlocks.java
+++ src/main/java/net/bikash/miningmod/block/ModBlocks.java
@@ -2,9 +2,11 @@ package net.bikash.miningmod.block;
 
 import net.bikash.miningmod.MiningMod;
 import net.bikash.miningmod.item.Moditems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,13 +23,12 @@ public class ModBlocks {
 
 
     public static final RegistryObject<Block> RUBY_ORE = registerBlock("ruby_ore",
-            ()-> new Block(BlockBehaviour.Properties.of()
+            ()-> new DropExperienceBlock (UniformInt.of(2,4), BlockBehaviour.Properties.of()
                     .strength(3f).requiresCorrectToolForDrops().sound( SoundType.STONE)));
 
-public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
-        () -> new Block(BlockBehaviour.Properties.of()
-                .strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
-
+    public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
+            ()-> new DropExperienceBlock (UniformInt.of(2,4), BlockBehaviour.Properties.of()
+                    .strength(3f).requiresCorrectToolForDrops().sound( SoundType.STONE)));
 
     public static  final  RegistryObject<Block> RUBY_BLOCK = registerBlock( "ruby_block" ,
             () -> new Block(BlockBehaviour.Properties.of()
@@ -37,6 +38,8 @@ public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire
     public static  final  RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock( "sapphire_block" ,
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST) ));
+
+
 
 
     private static<T extends Block> RegistryObject<T> registerBlock (String name, Supplier<T> block){
